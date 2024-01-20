2f8be0dc899e9edae34bcbd3383a62c5a8ec1c0e
Norton Andres Perez <nap1118@g.rit.edu>
2024-01-19 21:28:41 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let year = new Date().getFullYear();
+document.getElementById(
+  "copyright"
+).innerText = `Copyright \u00A9 ${year} Norton Andres Perez. All Rights Reserved.`;
+
+function shineIcons() {
+  /* make icons, buttons, and maybe a tags shine (should be random range(1min,5min))*/
+}
+
+function changeTheme() {
+  var theme = getCurrentTheme();
+}
+
+function getCurrentTheme() {
+  return;
+}
